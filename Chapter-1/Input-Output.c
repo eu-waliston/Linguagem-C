50aa9933a953d5f6ffeda0b8814f5da3d9f1ed4f
Waliston <goneisgone@hotmail.com>
2023-07-24 20:52:07 -0300

    Capitulo 1

--- Chapter-1/Input-Output.c
+++ Chapter-1/Input-Output.c
@@ -1,10 +1,75 @@
 #include <stdio.h>
+
+/*
+ * //File Copying
 int main() {
     int c;
     c = getchar();
     while( c != EOF) {
         putchar(c);
         c = getchar();
-        printf("%d", c);
+        //printf("%d", c);
     }
+}
+
+
+//Character Counting
+int main() {
+    long nc;
+
+    nc = 0;
+    while(getchar() != EOF) {
+        ++nc;
+        printf("%ld\n", nc);
+    }
+}
+
+
+
+//Line Counting
+int main () {
+    int c, nl;
+    nl = 0;
+    while((c = getchar()) != EOF)
+        if(c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+}
+
+
+
+// Line Counting
+int main () {
+    int c, nl;
+    nl = 0;
+    while((c == getchar()) != EOF) {
+        if(c == '\n') {
+            ++nl;
+        }
+        printf("%d\n", nl);
+    }
+}
+    */
+
+//Word Counting
+#define IN 1
+#define OUT 0
+
+int main() {
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while((c = getchar()) != EOF) {
+        nc++;
+        if(c == '\n') {
+            ++nl;
+        }
+        if(c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        } else if (state == OUT) {
+            state = IN;
+            nw++;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
 }
