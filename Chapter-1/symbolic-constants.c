5ab0e2c673cb0a75a692a8f5e01f7efb5efde644
Waliston <goneisgone@hotmail.com>
2023-07-24 20:36:46 -0300

    Capitulo 1

--- Chapter-1/symbolic-constants.c
+++ Chapter-1/symbolic-constants.c
@@ -1,3 +1,12 @@
-//
-// Created by we on 24/07/23.
-//
+#include <stdio.h>
+
+#define LOWER  0
+#define UPPER 300
+#define STEP 20
+
+int main() {
+    int fahr;
+    for(fahr = LOWER; fahr <= UPPER; fahr ++) {
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+}
