8b9fd87d3c54d1d3ad49c8b21999179b6907ede6
Waliston <goneisgone@hotmail.com>
2023-07-24 21:20:20 -0300

    Capitulo 1 >> Functions

--- Chapter-1/Functions.c
+++ Chapter-1/Functions.c
@@ -1,3 +1,13 @@
-//
-// Created by we on 24/07/23.
-//
+#include <stdio.h>
+int power(int m, int n) {
+    return m * n;
+};
+
+/* test power function */
+int main()
+{
+    int i;
+    for (i = 0; i < 10; ++i)
+        printf("%d\n", power(3, i) );
+    return 0;
+}
