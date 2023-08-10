f2440b13314a23845667ed9569d615c63c2f81ff
Waliston <goneisgone@hotmail.com>
2023-08-10 14:36:21 -0300

    C, Chapter 3 end

---
+++ Capitulo-3/operadores.c
@@ -0,0 +1,18 @@
+//Incremento e decremento
+#include <stdio.h>
+
+int main() {
+    int c;
+    
+    c = 5;
+    printf("%d\n", c);
+    printf("%d\n", c++);
+    printf("%d\n\n", c);
+
+    c = 5;
+    printf("%d\n", c);
+    printf("%d\n", ++c);
+    printf("%d\n", c);
+    
+    return 0;
+}
